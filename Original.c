7f84faeb23e686481da17739f4e21708d90db587
Lucasvinipl <lucasvinipl@outlook.com>
2017-09-30 10:01:36 -0300

    Primeiro Envio

---
+++ Original.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <locale.h>
+
+int main()
+{
+	//define o 
+	setlocale(LC_ALL,"portuguese");
+	
+	//declarando os ponteiros
+    char *pNome;
+    char *pTel;
+    int *pIdade;
+
+    pNome = malloc(50*sizeof(char));
+
+    pTel = malloc(14*sizeof(char));
+
+    pIdade = malloc(sizeof(int));
+    
+    printf("digite o nome......: ");
+    gets(pNome);
+
+    printf("digite a idade.....>");
+    scanf("%d", pIdade);
+
+    printf("Digite o Telefone: ");
+
+    fflush(stdin);
+
+    gets(pTel);
+
+    printf("\n\n---Listando os dados---\n");
+
+    puts(pNome);
+
+    printf("%d\n", *pIdade);
+
+    puts(pTel);
+
+    printf("\n---Listando os endereços de memoria---\n");
+    printf("pNome...: %p\n", pNome);
+    printf("pIdade...: %p\n", pIdade);
+    printf("pTel...: %p\n\n", pTel);
+
+    return  0;
+
+}
+
